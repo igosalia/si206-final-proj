b93d7df126a2d919dd98d5eed4846cb2d5292a75
Mengyi Li <mengyili@umich.edu>
2024-12-13 14:02:29 -0500

    performed linear regression

--- calculations_and_graphs.py
+++ calculations_and_graphs.py
@@ -4,6 +4,7 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as py
+import seaborn as sns
 
 database_path = "weather_and_horse_race_data.db"
 
@@ -36,6 +37,16 @@ def calculate_weather_info(data):
 
     return weather_stats
 
+def linreg(x, y):
+    #linear regression 
+    x = py.array(x)
+    y = py.array(y)
+    A = py.vstack([x, py.ones(len(x))]).T
+    slope, intercept = py.linalg.lstsq(A, y, rcond=None)[0]
+    return slope, intercept
+
+
+
 
 tracks = ["Bangor-on-Dee", "Cheltenham", "Doncaster", "Southwell (AW)", "Cork", "Dundalk (AW)", "Meydan", "San Isidro", "Eagle Farm", "Deauville", "Newcastle", "Wolverhampton (AW)", "Fairyhouse"]
 weather_tracks_map = {}
@@ -51,82 +62,6 @@ def main():
     #weather calculations
     weather_stats = calculate_weather_info(data)
     print(weather_stats)
-
-def calculate_avg_temps():
-    for track in tracks:
-        cur.execute("""
-            SELECT temperature FROM weather
-            WHERE track_name = ?
-        """, (track,))
-        temps = cur.fetchall()
-        num_days = len(temps)
-        if num_days > 0:
-            temp_sum = 0
-            for temp in temps:
-                temp_sum += temp[0]
-            weather_tracks_map[track] = {}
-            weather_tracks_map[track]["average_temp"] = temp_sum / num_days
-
-        else:
-            "Error getting data for this location"
-    
-
-def calculate_avg_humidity():
-    #calculate average humidity at each major racing course in DB
-    for track in tracks:
-        cur.execute("""
-            SELECT humidity FROM weather
-            WHERE track_name = ?
-        """, (track,))
-        values = cur.fetchall()
-        num_days = len(values)
-        if num_days > 0:
-            hum_sum = 0
-            for val in values:
-                hum_sum += val[0]
-            weather_tracks_map[track] = {}
-            weather_tracks_map[track]["average_humidity"] = hum_sum / num_days
-
-        else:
-            "Error getting data for this location"
-
-def calculate_avg_windspeed():
-    #calculate average windspeed at each major racing course in DB
-    for track in tracks:
-        cur.execute("""
-            SELECT windspeed FROM weather
-            WHERE track_name = ?
-        """, (track,))
-        values = cur.fetchall()
-        num_days = len(values)
-        if num_days > 0:
-            wind_sum = 0
-            for val in values:
-                wind_sum += val[0]
-            weather_tracks_map[track] = {}
-            weather_tracks_map[track]["average_windspeed"] = wind_sum / num_days
-
-        else:
-            "Error getting data for this location"
-
-def calculate_avg_visibility():
-    #calculate average visibility at each major racing course in DB
-    for track in tracks:
-        cur.execute("""
-            SELECT visibility FROM weather
-            WHERE track_name = ?
-        """, (track,))
-        values = cur.fetchall()
-        num_days = len(values)
-        if num_days > 0:
-            vis_sum = 0
-            for val in values:
-                vis_sum += val[0]
-            weather_tracks_map[track]["average_windspeed"] = vis_sum / num_days
-        else:
-            "Error getting data for this location"
-
-#TODO: Database Join calculation, more calculations and graphs
 
 if __name__ == "__main__":
     main()
